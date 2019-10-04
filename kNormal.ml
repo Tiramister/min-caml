de10458dd53eb193bbb208c066aebd4ea4701704
Tiramister <nh111424@gmail.com>
2019-10-04 17:49:28 +0900

    implement KNormal.print_knormal

--- kNormal.ml
+++ kNormal.ml
@@ -177,3 +177,85 @@ let rec g env = function (* K正規化ルーチン本体 (caml2html: knormal_g) 
                 (fun z -> Put(x, y, z), Type.Unit)))
 
 let f e = fst (g M.empty e)
+
+
+let rec indent n =
+  if (n = 0) then "" else ("  " ^ (indent (n - 1)))
+
+let rec print_knormal expr depth =
+  let print_var d (name, ty) =
+    (print_endline
+       ((indent d)
+        ^ name ^ " : " ^ (Type.string_of_type ty))) in
+
+  let print_func name exprs =
+    (print_endline ((indent depth) ^ name);
+     List.iter (fun e -> print_endline ((indent (depth + 1)) ^ e)) exprs) in
+
+  let print_let vars e1 e2 =
+    (print_endline ((indent depth) ^ "LET");
+     List.iter (print_var (depth + 1)) vars;
+     print_endline ((indent depth) ^ "=");
+     print_knormal e1 (depth + 1);
+     print_endline ((indent depth) ^ "IN");
+     print_knormal e2 (depth + 1)) in
+
+  let print_lettuple vars e1 e2 =
+    (print_endline ((indent depth) ^ "LET");
+     List.iter (print_var (depth + 1)) vars;
+     print_endline ((indent depth) ^ "=");
+     print_endline ((indent (depth + 1)) ^ e1);
+     print_endline ((indent depth) ^ "IN");
+     print_knormal e2 (depth + 1)) in
+  
+  match expr with
+  | Unit ->
+     print_func "UNIT" []
+  | Int n ->
+     print_func ((string_of_int n) ^ " : INT") []
+  | Float f ->
+     print_func ((string_of_float f) ^ " : FLOAT") []
+  | Neg e ->
+     print_func "NEG" [e]
+  | Add (e1, e2) ->
+     print_func "ADD" [e1; e2]
+  | Sub (e1, e2) ->
+     print_func "SUB" [e1; e2]
+  | FNeg e ->
+     print_func "FNEG" [e]
+  | FAdd (e1, e2) ->
+     print_func "FADD" [e1; e2]
+  | FSub (e1, e2) ->
+     print_func "FSUB" [e1; e2]
+  | FMul (e1, e2) ->
+     print_func "FMUL" [e1; e2]
+  | FDiv (e1, e2) ->
+     print_func "FDIV" [e1; e2]
+  | IfEq (e1, e2, et, ef) ->
+     (print_func "IFEQ" [e1; e2];
+      print_knormal et (depth + 1);
+      print_knormal ef (depth + 1))
+  | IfLE (e1, e2, et, ef) ->
+     (print_func "IFLE" [e1; e2];
+      print_knormal et (depth + 1);
+      print_knormal ef (depth + 1))
+  | Let (var, e1, e2) ->
+     print_let [var] e1 e2
+  | Var name ->
+     print_func ("VAR " ^ name) []
+  | LetRec (fdef, e) ->
+     print_let [fdef.name] fdef.body e
+  | App (e, es) ->
+     print_func "APP" (e :: es)
+  | Tuple es ->
+     print_func "TUPLE" es
+  | LetTuple (vars, e1, e2) ->
+     print_lettuple vars e1 e2
+  | Get (e1, e2) ->
+     print_func "GET" [e1; e2]
+  | Put (e1, e2, e3) ->
+     print_func "PUT" [e1; e2; e3]
+  | ExtArray e ->
+     print_func "EXTARRAY" [e]
+  | ExtFunApp (e, es) ->
+     print_func "EXTFUNAPP" (e :: es)
