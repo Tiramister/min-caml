5a61b670b2b114094d3b9ca0d61afe4cfed740e0
Tiramister <nh111424@gmail.com>
2019-10-04 17:07:14 +0900

    implement Type.string_of_type

--- type.ml
+++ type.ml
@@ -9,3 +9,25 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec string_of_type ty =
+  match ty with
+  | Unit  -> "UNIT"
+  | Bool  -> "BOOL"
+  | Int   -> "INT"
+  | Float -> "FLOAT"
+  | Fun (tys, ty') ->
+     let args =
+       (String.concat ", "
+          (List.map string_of_type tys)) in
+     "(" ^ args ^ ") -> " ^ (string_of_type ty')
+  | Tuple tys ->
+     let args =
+       (String.concat ", "
+          (List.map string_of_type tys)) in
+     "(" ^ args ^ ")"
+  | Array ty' -> "ARRAY OF " ^ (string_of_type ty')
+  | Var ty' ->
+     match !ty' with
+     | None   -> "UNKNOWN"
+     | Some t -> string_of_type t
